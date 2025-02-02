2d6593b07372a3c5e0c8202fa30cbe118165cf0d
jamesa261 <jamesalner@me.com>
2025-02-02 17:07:34 -0500

    WIP streamlit app comparison

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,33 +1,46 @@
+import os
+import sys
+
 import streamlit as st
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import torch
+import pandas as pd
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    return torch.device("cpu")
 
 st.title('Job Posting Search Engine')
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = get_device()
 
 # Function to load pre-computed embeddings
 @st.cache_resource
 def load_embeddings():
-    embeddings = np.load('embeddings.npy')
+    embeddings = np.load('fine_tuned_embeddings.npy')
     return embeddings
 
 # Function to load sentences
 @st.cache_resource
-def load_sentences():
-    with open('sentences.txt', 'r', encoding='utf-8') as f:
-        sentences = [line.strip() for line in f]
-    return sentences
+def load_job_postings():
+    job_postings_df = pd.read_parquet('data/job_postings.parquet')
+    job_postings_df['posting'] = job_postings_df['job_posting_title'] + ' @ ' + job_postings_df['company']
+    return job_postings_df['posting'].to_list()
+
 
 # Function to load the embedding model
 @st.cache_resource
 def load_model():
-    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', device=device)
-    return model
+    fine_tuned_model_path = os.path.join('data', 'fine_tuned_model')
+    fine_tuned_model = SentenceTransformer(fine_tuned_model_path, device=device)
+    return fine_tuned_model
 
 # Load resources
-embeddings = torch.tensor(load_embeddings(), device=device)
-sentences = load_sentences()
+embeddings = torch.tensor(load_embeddings()[:1000], device=device)
+job_postings = load_job_postings()[:1000]
 model = load_model()
 
 # User input
@@ -42,12 +55,12 @@ if user_input:
         similarities = torch.inner(input_embedding, embeddings)
 
         # Grab the top 5 most similar job posting titles
-        top5_indices = torch.argsort(similarities, descending=True)[:5]
+        top5_indices = torch.argsort(similarities, descending=True)[:10]
 
 
     # Display top 5 matching postings
-    st.subheader('Top 5 Matches:')
+    st.subheader('Top 10 Matches:')
     for idx in top5_indices:
-        st.write(f"**Job Posting:** {sentences[idx]}")
+        st.write(f"**Job Posting:** {job_postings[idx]}")
         st.write(f"**Similarity Score:** {similarities[idx]:.4f}")
         st.write('---')
--- streamlit_app/prepare_embeddings.py
+++ streamlit_app/prepare_embeddings.py
@@ -1,26 +1,54 @@
+import os
+import sys
+
 from sentence_transformers import SentenceTransformer
 import numpy as np
+import pandas as pd
 import torch
+from tqdm import trange
 
-# Sample sentences (replace this with your actual data)
-sentences = [
-    "The cat sits on the mat.",
-    "Dogs are man's best friend.",
-    "Artificial intelligence is the future.",
-    "I love reading books on machine learning.",
-    "Streamlit makes it easy to build web apps."
-]
+build_project_path = os.environ['BUILD_PROJECT_PATH']
+streamlit_app_data_path = os.path.join(build_project_path, 'streamlit_app', 'data')
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    return torch.device("cpu")
+
+job_postings_df = pd.read_parquet(os.path.join(streamlit_app_data_path, 'job_postings.parquet'))
+
+job_titles = job_postings_df['job_posting_title'].to_list()
+
+device = get_device()
 print(f'Using device: {device}')
 # Load the model
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', device=device)
+default_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', device=device)
+
+fine_tuned_model_path = os.path.join(streamlit_app_data_path, 'fine_tuned_model')
+fine_tuned_model = SentenceTransformer(fine_tuned_model_path, device=device)
 
 # Compute embeddings
-embeddings = model.encode(sentences, normalize_embeddings=True, convert_to_numpy=True)
+
+# process in chunks of 100
+default_embeddings = []
+fine_tuned_embeddings = []
+for i in trange(0, len(job_titles), 100):
+    chunk = job_titles[i:i+100]
+    default_embeddings.append(default_model.encode(chunk, normalize_embeddings=True, convert_to_numpy=True, device=device))
+    fine_tuned_embeddings.append(fine_tuned_model.encode(chunk, normalize_embeddings=True, convert_to_numpy=True, device=device))
+
+default_embeddings = np.concatenate(default_embeddings)
+print(default_embeddings.shape)
+fine_tuned_embeddings = np.concatenate(fine_tuned_embeddings)
+print(fine_tuned_embeddings.shape)
 
 # Save embeddings and sentences
-np.save('embeddings.npy', embeddings)
-with open('sentences.txt', 'w', encoding='utf-8') as f:
-    for sentence in sentences:
-        f.write(f"{sentence}\n")
+np.save(os.path.join(streamlit_app_data_path, 'default_embeddings.npy'), default_embeddings)
+np.save(os.path.join(streamlit_app_data_path, 'fine_tuned_embeddings.npy'), fine_tuned_embeddings)
+
+
+# with open('sentences.txt', 'w', encoding='utf-8') as f:
+#     for sentence in sentences:
+#         f.write(f"{sentence}\n")

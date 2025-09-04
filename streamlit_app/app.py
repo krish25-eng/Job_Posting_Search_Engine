58db775473003c923b5d04aae59bdbb10c93b10e
krish25-eng <krish.vardhini@gmail.com>
2025-09-04 14:36:29 -0400

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -64,18 +64,18 @@ def load_fine_tuned_embeddings():
 
 @st.cache_resource
 def load_default_embeddings():
-    embeddings = np.load(os.path.join('data', 'default_embeddings.npy'))
+    embeddings = np.load(os.path.join('streamlit_app','data', 'default_embeddings.npy'))
     return embeddings
 
 @st.cache_resource
 def load_job_postings():
-    job_postings_df = pd.read_parquet(os.path.join('data', 'job_postings.parquet'))
+    job_postings_df = pd.read_parquet(os.path.join('streamlit_app','data', 'job_postings.parquet'))
     job_postings_df['posting'] = job_postings_df['job_posting_title'] + ' @ ' + job_postings_df['company']
     return job_postings_df['posting'].to_list()
 
 @st.cache_resource
 def load_fine_tuned_model():
-    fine_tuned_model_path = os.path.join('data', 'fine_tuned_model')
+    fine_tuned_model_path = os.path.join('streamlit_app','data', 'fine_tuned_model')
     model = SentenceTransformer(fine_tuned_model_path, device=device)
     return model
 
